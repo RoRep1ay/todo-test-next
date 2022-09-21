38a678e1998f2dc5d68094cfb4019e029424fd97
Sojimmy Nea <sojimmy.nea@okoone.com>
2022-09-21 11:31:31 +0700

    update

--- models/constants.ts
+++ models/constants.ts
@@ -1,2 +1,2 @@
-export const URL_ENDPOINT = `${ process.env.BASE_URL }`;
+export const URL_ENDPOINT = `${ process.env.NEXT_PUBLIC_BASE_URL }`;
 export const API_ENDPOINT = `${URL_ENDPOINT}/api/todos`;
