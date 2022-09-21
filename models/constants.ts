51b6764c90796eecc434a88eaefa7db160fd23fe
Sojimmy Nea <sojimmy.nea@okoone.com>
2022-09-21 12:14:43 +0700

    update env variable

--- models/constants.ts
+++ models/constants.ts
@@ -1,2 +1,2 @@
-export const URL_ENDPOINT = `${ process.env.NEXT_PUBLIC_BASE_URL }`;
+export const URL_ENDPOINT = `${ process.env.DEPLOY_URL || process.env.NEXT_PUBLIC_BASE_URL }`;
 export const API_ENDPOINT = `${URL_ENDPOINT}/api/todos`;
